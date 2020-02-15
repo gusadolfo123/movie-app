b85886b153c106dd1c72d43b90842d23644dad82
gusadolfo123 <gusadolfo123@hotmail.com>
2020-02-15 14:41:34 -0500

    Implementando middleware de log de errores

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-
 const moviesApi = require('./routes/movies');
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
 
 // MIDDLEWARES
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 moviesApi(app);
+
+// MIDDLEWARES
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
